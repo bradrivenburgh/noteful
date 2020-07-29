afeaedd44366e040adc8f66292025cb4301b9de2
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-07-29 17:15:27 -0400

    cleaning up Note component

--- src/note/Note.js
+++ src/note/Note.js
@@ -3,11 +3,10 @@ import moment from 'moment';
 
 function Note({note}) {
   const date = moment(note.modified).format("do MMM YYYY");
-  console.log(date) 
   return (
     <div className="Note_note-item">
       <h2>{note.name}</h2>
-      <span>Last modified on {date}</span>
+      <span>Last modified on {date} </span>
       <button>Delete Note</button>
     </div>
   );
