559ce400dcd202965abb7db65ccd227006ad2f7d
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-10-08 11:33:23 -0400

    changed data keys in the test fixtures to match the modified data keys

--- src/store-for-tests.js
+++ src/store-for-tests.js
@@ -1,74 +1,46 @@
 // These arrays should be used for tests in App.test.js
 export const notes = [
   {
-    "id": "d26e01a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Pigs",
+    "id": 1,
+    "noteName": "Pigs",
     "modified": "2018-03-01T00:00:00.000Z",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+    "folderId": 1,
     "content": "Ea illum est et sit minus voluptatibus suscipit non quos. Qui esse asperiores ex est voluptatem sit accusantium vel. Nobis et quam. Non voluptate sint veniam est illo ipsa ea necessitatibus dolor. Ea praesentium necessitatibus quia pariatur. Nihil sequi nobis tempore aliquam sapiente.\n \rVoluptatem unde eveniet maiores. Vitae repudiandae cupiditate atque aperiam. Aut dolorum soluta dolorem molestiae dolor. Dolor sit velit delectus numquam ea.\n \rConsequatur atque velit nam similique aut sunt mollitia occaecati laborum. Possimus quo consequatur sed. Minima voluptas perferendis qui quos omnis. Natus similique nihil ea porro. Iure officiis sed rerum ut error necessitatibus."
   },
   {
-    "id": "d26e0714-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Bears",
+    "id": 2,
+    "noteName": "Bears",
     "modified": "2018-07-12T23:00:00.000Z",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+    "folderId": 1,
     "content": "Ullam quis minus iste magni error maiores et. Nihil mollitia eius quisquam non in. At ullam neque architecto quod ipsam eos atque esse.\n \rQuis eos est laborum officiis. Alias quaerat iusto tempore aliquid dolorem. Et ullam omnis dicta sit dolore cumque at. Sint ut blanditiis totam suscipit laudantium tenetur et sit. Molestiae cumque fuga quidem sint animi iusto. Dolores temporibus nostrum totam qui non quia consequuntur.\n \rExcepturi quisquam et minima illo ut perferendis alias nemo. Temporibus fuga distinctio repellendus culpa odit non alias placeat. Aliquid facilis mollitia."
   },
   {
-    "id": "d26e0854-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Horses",
+    "id": 3,
+    "noteName": "Horses",
     "modified": "2018-08-20T23:00:00.000Z",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+    "folderId": 1,
     "content": "Modi asperiores cumque excepturi quas. Neque quisquam neque in doloremque dicta asperiores. Quia dolorem omnis illum aut. Qui iure et provident quae ad et voluptatem nisi cum. Veniam officia et officia omnis et aut et.\n \rEx dicta hic voluptatem laborum adipisci quae modi earum. Suscipit aut est et. Eum qui consequatur tempora asperiores veniam distinctio in. Recusandae laboriosam non. Qui earum doloribus.\n \rIncidunt sapiente incidunt debitis rem nobis unde voluptatem et. Rem dicta quas et qui fuga in voluptas assumenda quo. Dolor quas voluptas doloribus."
   },
   {
-    "id": "d26e0bce-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Elephants",
+    "id": 4,
+    "noteName": "Elephants",
     "modified": "2018-04-11T23:00:00.000Z",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
+    "folderId": 1,
     "content": "Omnis accusamus et quasi culpa. Consequatur voluptatem quia sint quia eligendi. Provident est delectus occaecati voluptates perferendis et repudiandae. Qui similique repellat fugit nostrum aut excepturi est est. Ratione perspiciatis eaque aliquam tempore nihil. Illo rerum nihil eius dolorum quia asperiores.\n \rTempora earum repellat aliquid iure dolorem. Vel tempora maxime voluptates est architecto ex. Qui quia ab.\n \rEt qui a nihil in voluptate perspiciatis asperiores qui. Occaecati quia aut. Culpa est in distinctio. Quia velit soluta et deleniti perspiciatis assumenda maiores. Quae delectus voluptas nisi explicabo aut ea sit. Non in voluptates harum nulla aut est sed tenetur."
   },
-  {
-    "id": "d26e0f48-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Lions",
-    "modified": "2018-04-26T23:00:00.000Z",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "Quaerat asperiores corrupti vel eveniet aut ut. Qui non facilis. Consectetur hic ducimus aut. Fugiat et ea autem officia ad repellat.\n \rSint quia esse corporis eius minus dicta soluta veniam. Eius dolorem fugiat corrupti. Inventore quasi est illo in. Harum exercitationem eveniet ut nisi dolore minima. Alias nostrum consequatur et nostrum voluptate earum dolorem odit qui.\n \rCommodi optio occaecati qui modi. Maiores illo consequatur nihil sed eius. Sed assumenda voluptas recusandae hic."
-  },
-  {
-    "id": "d26e11a0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Bats",
-    "modified": "2018-12-01T00:00:00.000Z",
-    "folderId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "Alias non fuga dolore sunt numquam vitae dolores laudantium. Explicabo voluptate atque assumenda commodi eum illum accusamus architecto. Illum sequi sed perferendis non voluptate quia sint laboriosam. Quo dolore tempore eveniet est dolorem quod.\n \rHarum expedita accusantium animi eligendi molestiae et delectus. Consectetur voluptatem accusantium expedita esse eveniet pariatur libero non et. Voluptas eligendi occaecati ipsum nemo vel. Et id perferendis distinctio laborum temporibus. Aut vel hic. Quo aut ipsam enim cum.\n \rEarum voluptate tempore. Suscipit est architecto eum laudantium quas harum. Est voluptate tenetur id tempore. Alias eaque aut tempora non soluta quos maxime."
-  },
-  {
-    "id": "d26e12c2-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Turtles",
-    "modified": "2018-09-11T23:00:00.000Z",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "Magnam est quaerat sit numquam accusamus cum. Dolor impedit autem excepturi possimus eum accusamus odit. Tenetur non cumque atque ea magni et perferendis sit. Vero rerum consequatur sit recusandae. Fugiat earum et.\n \rQuia exercitationem nisi nulla omnis culpa quia. Ea harum eos. Et voluptatibus nesciunt eaque illo. Atque autem unde accusantium.\n \rQuod minima delectus. Vel veritatis eius voluptatem qui dolor cupiditate adipisci. Deserunt ut quia amet."
-  },
-  {
-    "id": "d26e1452-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Zebras",
-    "modified": "2018-08-13T23:00:00.000Z",
-    "folderId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "content": "Quos sint labore. Quaerat enim ut quisquam voluptatibus excepturi aut laboriosam. Molestiae qui facere est at error porro inventore quam omnis. Ducimus in vel quo hic velit doloremque.\n \rAsperiores quisquam quae assumenda est pariatur voluptatem aut. Fuga et eos praesentium asperiores in quia labore quod tenetur. Accusantium perferendis magni in deserunt aut debitis non repellat. Aperiam quia voluptatum. Quas repellendus impedit eos veniam repellat.\n \rDignissimos omnis quasi enim labore aliquid est nihil. Sed excepturi ex et aut error dolorum placeat et. Itaque sunt sint dolorum exercitationem aspernatur quia sunt animi."
-  }
 ]
 
 export const folders = [
   {
-    "id": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Important"
+    "id": 1,
+    "folderName": "Important"
   },
   {
-    "id": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Super"
+    "id": 2,
+    "folderName": "Super"
   },
   {
-    "id": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    "name": "Spangley"
+    "id": 3,
+    "folderName": "Spangley"
   }
 ]
