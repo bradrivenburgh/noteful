cbb0c0eede7fad056f2ea00227557a5ce6e3a4b4
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-10-10 11:26:01 -0400

    added conditional statement in config for API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
+let API_ENDPOINT = "";
+
+(process.env.NODE_ENV === 'development')
+  ? API_ENDPOINT = "http://localhost:8000/api/noteful"
+  : API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "https://vast-caverns-17279.herokuapp.com/api/noteful",
+  API_ENDPOINT,
   API_KEY: process.env.REACT_APP_API_KEY
 }
